c5577b7e658c5889d7c821374727aea48d04d4a1
Charles Sebold <csebold@gmail.com>
2025-04-01 15:58:21 -0500

    Leaving deploy half done for now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+set +x
 source ./scripts/private.sh
+echo "PLUGINDIR = $PLUGINDIR"
+PLUGINDIR=$(readlink -f "$PLUGINDIR")
+echo "  which is now $PLUGINDIR"
+DESTDIR="$PLUGINDIR/$TARGET"
 if [ "$DESTDIR" != "" ] ; then
 	COMMANDS=("$@")
 	WORKDIR="$DESTDIR"
 else
 	PARMS=("$@")
 	COMMANDS=("${PARMS[@]:0:${#PARMS[@]}-1}")
-	WORKDIR="${PARMS[@]: -1}"
+	WORKDIR="$(realpath ${PARMS[@]: -1})"
 fi
+
 echo "Deploying into $WORKDIR"
 # echo "COMMANDS = ${COMMANDS[@]}"
 # echo "WORKDIR = $WORKDIR"
